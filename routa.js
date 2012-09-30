59c14b5a86ddeaf6b198550f74351901c6f895a4
Yonatan Ellman <jellman85@gmail.com>
2012-09-30 10:39:59 +0200

    refactor use res.json instead of setting headers and pass the req and res direct

--- routa.js
+++ routa.js
@@ -10,22 +10,25 @@ exports.createRoute = function (app, options) {
   var route = options.route;
   var RouteFunc = options.func;
   var RouteData = options.data;
-  var RouteStructure = buildRouteStructure(options);  
+  var RouteStructure = _buildRouteStructure(options);  
 
   app.get(options.route, function(req, res, next) {    
-  var params = require('querystring').parse(req.params[RouteData]);                         
-  
-  var args = new Array(params, this.dbConnection, defineJSONCallback(res));
-  routa({
-    route:root+buildRoutePath(req.params, RouteStructure),
-    func:req.params[RouteFunc],
-    args:args,
-    error:next
-    });     
-});
+    var params = require('querystring').parse(req.params[RouteData]);                         
+
+    var args = new Array(req, res, next, {    
+      db:dbConnection
+    });
+
+    _routa({
+      route:root+_buildRoutePath(req.params, RouteStructure),
+      func:req.params[RouteFunc],
+      args:args,
+      next:next
+      });     
+    });
 }
 
-function buildRouteStructure(options) {
+function _buildRouteStructure(options) {
   var structure = [];
   var params = options.route.split('/:');
   params.shift();
@@ -37,31 +40,20 @@ function buildRouteStructure(options) {
   return (structure);
 }
 
-function buildRoutePath(params, RouteStructure) {
+function _buildRoutePath(params, RouteStructure) {
   var path = '';
   for (var index in RouteStructure) {       
     path+= '/'+params[RouteStructure[index]];
   }
   return(path);
 }
-//set the headers for json and utf-8
-function setJSONHeaders(res) {
-  res.header('Content-Type', 'application/json');
-  res.header('Charset', 'utf-8');  
-}
-
-//build the callback functiona and pass along
-function defineJSONCallback(res) {
-  setJSONHeaders(res);
-  return function(ret) {res.send(ret)};
-}
 
 //require and run the appropriate function
-function routa(options) {
+function _routa(options) {
   try {
     require(options.route)[options.func].apply(this, options.args);
   } catch (e) {        
-    options.error();   
+    options.next();   
   } 
 }
 
